6e39f58036c952988bcde5e8320f0a50f6354001
Enochboakye <113483465+Enochboakye@users.noreply.github.com>
2025-06-18 00:17:37 +0000

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,11 @@ import Splash from "@/components/ui/welcome-message";
 import { useEffect } from "react";
 import { redirect } from "next/navigation";
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import { useAuth } from '@clerk/nextjs'
+
 
 export default function Home() {
-const { isSignedIn } = useAuth()
-  const router = useRouter()
 
-  useEffect(() => {
-    if (!isSignedIn) {
-      router.push('/verify-officer')
-    }
-  }, [isSignedIn, router])
+
 
   useEffect(() => {
     // Simulate a loading state
