be04e1ab0fc2f97ef65f08646023e997c4c12078
Enochboakye <113483465+Enochboakye@users.noreply.github.com>
2025-07-14 08:43:34 +0000

    initial commit

--- src/components/ui/topbar.tsx
+++ src/components/ui/topbar.tsx
@@ -14,7 +14,7 @@ const Topbar = () => {
   const router = useRouter();
   
   return (
-    <div className='flex justify-between items-center p-4  shadow-md w-4xl fixed top-1'>
+    <div className='flex justify-between items-center p-4  shadow-md w-4xl fixed top-1 mb-4  bg-gray-400'>
       <div className=" flex items-center space-x-4">
         <SidebarTrigger />
         <ModeToggle/>
@@ -23,7 +23,7 @@ const Topbar = () => {
         </Button>
          
       </div>
-      <div><h1 className='text-2xl font-bold text-blue-700 text-shadow-2xs text-shadow-sky-300'>ANTI ARMED ROBBERY UNIT (C.I.D HQRS)</h1></div>
+      <div><h1 className='text-2xl font-bold text-indigo-900 text-shadow-2xs text-shadow-sky-300'>ANTI ARMED ROBBERY UNIT (C.I.D HQRS)</h1></div>
       <div className="flex items-center gap-8">
        <div>
         <Button onClick={()=> router.forward()}>
